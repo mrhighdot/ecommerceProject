c04f7c01c2ab2c49a33117275209bcd7876d60db
Gabriel Omane - Yeboah <gabrielomane@outlook.com>
2023-12-23 12:01:17 +0000

    Started work with the cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from "react";
 
 const Cart = () => {
+  const [selected, setSelected] = useState(false);
   return (
-    <div>Cart</div>
-  )
-}
+    <section>
+      <h1 className="text-4xl mb-4">Shopping Cart</h1>
+      {/* Horizontal Line */}
+      <div className=" mb-4"></div>
+      {/* Cart Item */}
+      <section className="mb-4 border-y border-y-slate-200 py-4">
+        <input
+          type="checkbox"
+          name="checkbox"
+          id="checkbox"
+          value={selected}
+          onChange={() => setSelected(!selected)}
+        />
+      </section>
+    </section>
+  );
+};
 
-export default Cart
+export default Cart;
